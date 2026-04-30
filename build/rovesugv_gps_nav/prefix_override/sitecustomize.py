c7f3e5a86606dec7e5959fb19315624bace48451
hhaka <hannu.hakalahti@seamk.fi>
2026-04-30 14:46:33 +0300

    install, build, and log folders removed

--- build/rovesugv_gps_nav/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/kayttaja/rovesugv/ros_ws/src/rovesugv_gps_nav/install/rovesugv_gps_nav'
